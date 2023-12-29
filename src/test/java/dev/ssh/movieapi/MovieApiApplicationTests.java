7ecf88f83bfdaa693a1110b465d74524c3095c1c
SudarshanShah <sudarshanshah911@gmail.com>
2023-12-29 18:51:23 +0530

    project setup

---
+++ src/test/java/dev/ssh/movieapi/MovieApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ssh.movieapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
